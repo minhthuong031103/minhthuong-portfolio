e5dc62d078ac04ac684652a44c6ff733750d33d2
minhthuong031103 <nominh67@gmail.com>
2023-06-11 22:42:13 +0700

    xam

--- app/components/ImageCovers.js
+++ app/components/ImageCovers.js
@@ -1,5 +1,6 @@
 import image_id_3 from '../../public/blog_images/blog_id_3.png';
 import image_id_8 from '../../public/project_images/musicplayer.png';
+import image_id_9 from '../../public/blog_images/blog_id_9.png';
 export const ImageCover = [
   {
     id: 3,
@@ -11,6 +12,6 @@ export const ImageCover = [
   },
   {
     id: 9,
-    cover: image_id_8,
+    cover: image_id_9,
   },
 ];
