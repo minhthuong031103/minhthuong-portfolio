62528695c0238bb1a739ee8348f7e9145ef181cc
minhthuong031103 <nominh67@gmail.com>
2023-06-16 16:20:35 +0700

    add blog 11 12

--- app/components/ImageCovers.js
+++ app/components/ImageCovers.js
@@ -2,6 +2,8 @@ import image_id_3 from '../../public/blog_images/blog_id_3.png';
 import image_id_8 from '../../public/project_images/musicplayer.png';
 import image_id_9 from '../../public/blog_images/blog_id_9.png';
 import image_id_10 from '../../public/blog_images/blog_id_10.jpg';
+import image_id_11 from '../../public/blog_images/blog_id_11.png';
+import image_id_12 from '../../public/blog_images/blog_id_12.png';
 export const ImageCover = [
   {
     id: 3,
@@ -18,5 +20,13 @@ export const ImageCover = [
   {
     id: 10,
     cover: image_id_10,
+  },
+  {
+    id: 11,
+    cover: image_id_11,
+  },
+  {
+    id: 12,
+    cover: image_id_12,
   },
 ];
